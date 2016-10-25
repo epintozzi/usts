f940b01859948cda0ba5ed67c445848cfb5ee518
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-25 12:05:20 -0600

    Add ability for user to create/update account info
    Test user's ability to create and edit account

---
+++ spec/features/users/user_can_create_account_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "User can create account" do
+  scenario "a user can create a new account" do
+    first_name = "Erin"
+    last_name = "Pintozzi"
+    email = "erin@pintozzi.com"
+    password = "helloworld"
+    boat_number = "V26"
+    usts_number = "12345"
+
+    visit '/users/new'
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[boat_number]', with: boat_number
+    fill_in 'user[usts_number]', with: usts_number
+    click_on "Create User"
+
+    expect(page).to have_content first_name
+    expect(page).to have_content last_name
+    expect(page).to have_content email
+    expect(page).to have_content boat_number
+    expect(page).to have_content usts_number
+  end
+end
---
+++ spec/features/users/user_can_edit_account_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "User can edit profile" do
+  scenario "A user can visit their profile page and edit it" do
+    user = User.create(first_name: "Erin", last_name: "Pintozzi", email: "erin@pintozzi.com", password: "helloworld", boat_number: "V26", usts_number: "12345")
+
+    new_boat_number = "V36"
+    new_last_name = "Barth"
+
+    visit user_path(user)
+    click_on "Edit Profile"
+    fill_in 'user[boat_number]', with: new_boat_number
+    fill_in 'user[last_name]', with: new_last_name
+    fill_in 'user[password]', with: user.password
+    click_on "Update User"
+
+    expect(page).to have_content new_boat_number
+    expect(page).to have_content new_last_name
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.email
+    expect(page).to have_content user.usts_number
+  end
+end
