4d9bf1d1448197c0ad2af72e8032e6ad876af43c
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-27 15:24:13 -0600

    Finish tests for admin functionality of races

--- spec/features/races/admin_can_update_a_race_spec.rb
+++ spec/features/races/admin_can_update_a_race_spec.rb
@@ -1,39 +1,37 @@
 require 'rails_helper'
 
-describe "Admin can create a race" do
+describe "Admin can update a race" do
   context "as an admin" do
-    scenario "an admin can enter data to create a new race" do
+    scenario "an admin can enter data to update an existing race" do
       admin = create(:admin)
-      address = "202 W 1st St"
+      race = create(:race)
+
+      address = "200 W 1st St"
       city = "DePue"
-      state = "IL"
-      start_date = '2017/07/26'
-      end_date = '2017/07/30'
+      start_date = '2017/07/25'
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit '/admin/races/new'
+      visit "/admin/races/#{race.id}/edit"
       fill_in 'race[address]', with: address
       fill_in 'race[city]', with: city
-      fill_in 'race[state]', with: state
-      fill_in 'race[start_date]', with: start_date
-      fill_in 'race[end_date]', with: end_date
-      click_on "Create Race"
+      click_on "Update Race"
 
       expect(page).to have_content address
       expect(page).to have_content city
-      expect(page).to have_content state
+      expect(page).to_not have_content "Yemen"
     end
   end
 
   context "as a default user" do
     scenario "they are shown a 404" do
       user = create(:user)
+      race = create(:race)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit '/admin/races/new'
-      expect(page).to_not have_content("Create a new race")
+      visit "/admin/races/#{race.id}/edit"
+      expect(page).to_not have_content("Edit #{race.city} race")
       expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end
