0a4359376e6ba6ce3d955349380f766ec5fb3f6a
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-31 09:02:39 -0600

    minor styling to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,8 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @races = Race.all
     @users = User.all
+    @boat_classes = BoatClass.all
     @registrations = Registration.all.group(:boat_class).count
-    
+
   end
 end
